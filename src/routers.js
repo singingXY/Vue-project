8c12247731a82a7c4b873bda3894a9196065a0ba
singingXY <singeve@qq.com>
2019-08-27 11:47:52 +0800

    关于Loading chunk {n} failed 错误，去掉路由懒加载

--- src/routers.js
+++ src/routers.js
@@ -1,44 +1,52 @@
 import Vue from 'vue/dist/vue.js'
 import VueRouter from 'vue-router'
 import Index from './views/list.vue'
-const Article = () => import('./views/article.vue')
-const User = () => import('./views/user.vue')
-const Collect = () => import('./views/collect.vue')
-const Message = () => import('./views/message.vue')
-const About = () => import('./views/about.vue')
+import Article from './views/article.vue'
+import User from './views/user.vue'
+import Collect from './views/collect.vue'
+import Message from './views/message.vue'
+import About from './views/about.vue'
 
 // 安装插件
 // 挂载属性
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  name: 'index',
-  component: Index
-}, {
-  path: '/topic/:id',
-  name: 'article',
-  component: Article
-}, {
-  path: '/user/:loginname',
-  name: 'user',
-  component: User
-}, {
-  path: '/user/:loginname/collections',
-  name: 'collect',
-  component: Collect
-}, {
-  path: '/message',
-  name: 'message',
-  component: Message
-}, {
-  path: '/about',
-  name: 'about',
-  component: About
-}, {
-  path: '*',
-  component: Index
-}]
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/topic/:id',
+    name: 'article',
+    component: Article
+  },
+  {
+    path: '/user/:loginname',
+    name: 'user',
+    component: User
+  },
+  {
+    path: '/user/:loginname/collections',
+    name: 'collect',
+    component: Collect
+  },
+  {
+    path: '/message',
+    name: 'message',
+    component: Message
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '*',
+    component: Index
+  }
+]
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 export default new VueRouter({
